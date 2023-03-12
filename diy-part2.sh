89f5f0ecb7647eb4a8c703771fd47af553ef26fe
Ne <3661428@gmail.com>
2023-03-13 02:30:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,17 +22,17 @@
 # rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 # ##---------------------------------------------------------
 
-##----------------- clash ------------------
-curl -sL -m 30 --retry 2 https://github.com/vi-si/Redmi-Ax6000/blob/main/core/clash -o /tmp/clash
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-##---------------------------------------------------------
+# ##----------------- clash ------------------
+# curl -sL -m 30 --retry 2 https://github.com/vi-si/Redmi-Ax6000/blob/main/core/clash -o /tmp/clash
+# chmod +x /tmp/clash >/dev/null 2>&1
+# mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+# mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+# ##---------------------------------------------------------
 
-##----------------- clash_tun ------------------
-curl -sL -m 30 --retry 2 https://github.com/vi-si/Redmi-Ax6000/blob/main/core/clash_tun -o /tmp/clash_tun
-chmod +x /tmp/clash_tun >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash_tun feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
-##---------------------------------------------------------
+# ##----------------- clash_tun ------------------
+# curl -sL -m 30 --retry 2 https://github.com/vi-si/Redmi-Ax6000/blob/main/core/clash_tun -o /tmp/clash_tun
+# chmod +x /tmp/clash_tun >/dev/null 2>&1
+# mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+# mv /tmp/clash_tun feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
+# ##---------------------------------------------------------
 
